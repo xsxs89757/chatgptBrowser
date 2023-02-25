2fd84da8f854c96b94110484cbf1c1eab51d5dc3
wanglei <317448423@qq.com>
2023-02-25 16:21:54 +0800

    fix bug

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 import fastify from 'fastify';
-import { ChatGPTAPIBrowser } from 'chatgpt';
+import { ChatGPTUnofficialProxyAPI } from 'chatgpt';
 import fs from 'fs';
 import { pathToFileURL } from 'url'
+import request from "superagent";
 
 const arg = process.argv.find((arg) => arg.startsWith('--settings'));
 let path;
@@ -26,8 +27,6 @@ if (fs.existsSync(path)) {
     process.exit(1);
 }
 
-const accounts = [];
-const conversationsMap = {};
 const base =  `chatgpt{id}@chatgptbatch.uu.me`;
 const accountsMapArr = settings.index.split('-');
 
@@ -38,84 +37,74 @@ for(let i = parseInt(accountsMapArr[0]);i<= parseInt(accountsMapArr[1]);i++) {
     })
 }
 
-for (let i = 0; i < settings.accounts.length; i++) {
-    const account = settings.accounts[i];
-    const api = new ChatGPTAPIBrowser({
-        ...account,
-        nopechaKey: account.nopechaKey || settings.nopechaKey || undefined,
-        captchaToken: account.twoCaptchaKey || settings.twoCaptchaKey || undefined,
-        // For backwards compatibility
-        proxyServer: account.proxyServer || account.proxy || undefined,
-    });
+let _accessToken = null,_replyAccessToken = 0
 
-    await api.initSession().then(() => {
-        console.log(`Session initialized for account ${i}.`);
-        accounts.push(api);
-    });
-
-    // call `api.refreshSession()` every hour to refresh the session
-    setInterval(() => {
-        api.refreshSession().then(() => {
-            console.log(`Session refreshed for account ${i}.`);
-        });
-    }, 30 * 60 * 1000);
-
-    // call `api.resetSession()` every 24 hours to reset the session
-    setInterval(() => {
-        api.resetSession().then(() => {
-            console.log(`Session reset for account ${i}.`);
-        });
-    }, 24 * 60 * 60 * 1000);
+const getAccessToken = async() => {
+    const index = Math.floor((Math.random()* settings.accounts.length))
+    const result = await request.post('http://43.153.18.225:5000')
+                    .send({u: settings.accounts[index].email,p: settings.accounts[index].password})
+    if(result.body.code === 0){
+        _accessToken = result.body.access_token
+    }else{
+        _accessToken = null
+    }
 }
 
-let currentAccountIndex = 0;
+const replyAccessToken = async() => {
+    if(_replyAccessToken <= 3 ){
+        _replyAccessToken++
+        await getAccessToken()
+    }
+}
+
+await getAccessToken()
+setInterval( async()=>{
+    await getAccessToken()
+}, 4 * 60 * 60 * 1000)
+
 
 const server = fastify();
 
 server.post('/chatgpt', async (request, reply) => {
-    if (accounts.length === 0) {
-        reply.send({ code: 1, msg: 'No sessions available.' });
-        return;
-    }
     const subject = request.body.subject
     if(!subject){
         return reply.send({ code: 1, msg: 'subject error' })
     }
-
+    if(_accessToken === null){
+        return res.json({ code: 1, msg: 'accessToken error' })
+    }
     const conversationId = request.body.conversation_id ? request.body.conversation_id.toString() : undefined;
 
-    // Conversation IDs are tied to accounts, so we need to make sure that the same account is used for the same conversation.
-    if (conversationId && conversationsMap[conversationId]) {
-        // If the conversation ID is already in the map, use the account that was used for that conversation.
-        currentAccountIndex = conversationsMap[conversationId];
-    } else {
-        // If the conversation ID is not in the map, use the next account.
-        currentAccountIndex = (currentAccountIndex + 1) % accounts.length;
-    }
-    
+    const proxyUrl = [
+        'https://chat.duti.tech/api/conversation',
+        'https://gpt.pawan.krd/backend-api/conversation'
+    ]
+    const index = Math.floor((Math.random()*proxyUrl.length))
+
     let result;
     let error;
-    try {
+    try{
         const parentMessageId = request.body.parent_message_id ? request.body.parent_message_id.toString() : undefined;
-        result = await accounts[currentAccountIndex].sendMessage(subject, {
+        const api = new ChatGPTUnofficialProxyAPI({
+            accessToken: _accessToken,
+            apiReverseProxyUrl: proxyUrl[index]
+        })
+        result = await api.sendMessage(subject, {
             conversationId,
             parentMessageId,
+            timeoutMs: 3 * 60 * 1000, 
         });
-        // ChatGPT ends its response with a newline character, so we need to remove it.
-        result.response = result.response.trim();
-        if (conversationId) {
-            // Save the account index for this conversation.
-            conversationsMap[conversationId] = currentAccountIndex;
-        }
+
     } catch (e) {
         error = e;
     }
 
     if (result !== undefined) {
         reply.send({
-            content : result.response,
+            content : result.text,
             conversation_id: result.conversationId,
-            parent_message_id : result.messageId,
+            parent_message_id : result.parentMessageId,
+            server: index
         });
     } else {
         console.error(error);
--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 export default {
     accounts: [
     ],
-    index: '1-1',
+    index: '1-10',
     // The port the server will run on (optional, defaults to 3000)
     port: 8788,
     // Your NopeCHA API key. This will be applied to all accounts but can be overridden on a per-account basis.
     nopechaKey: '',
     // Your 2Captcha API key. This will be applied to all accounts but can be overridden on a per-account basis.
-    twoCaptchaKey: '499ac194a4f199e08e3eee7184bd74d1',
+    twoCaptchaKey: '499ac194a4f199e08e3eee7184bd74d1'
 }
