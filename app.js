c3f6b47458a52cbfa57e1e5d94ae02b87e7999d0
wanglei <317448423@qq.com>
2023-03-04 13:55:12 +0800

    fix bug

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import fastify from 'fastify';
-import { ChatGPTUnofficialProxyAPI } from 'chatgpt';
+import { ChatGPTAPI } from 'chatgpt';
 import fs from 'fs';
 import { pathToFileURL } from 'url'
-import request from "superagent";
+import ExpiryMap from 'expiry-map'
 
 const arg = process.argv.find((arg) => arg.startsWith('--settings'));
 let path;
@@ -27,41 +27,11 @@ if (fs.existsSync(path)) {
     process.exit(1);
 }
 
-const base =  `chatgpt{id}@chatgptbatch.uu.me`;
-const accountsMapArr = settings.index.split('-');
-
-for(let i = parseInt(accountsMapArr[0]);i<= parseInt(accountsMapArr[1]);i++) {
-    settings.accounts.push({
-        email: base.replace('{id}', i),
-        password: 'qazwsx123!@#'
-    })
-}
-
-let _accessToken = null,_replyAccessToken = 0
-
-const getAccessToken = async() => {
-    const index = Math.floor((Math.random()* settings.accounts.length))
-    const result = await request.post('http://43.153.18.225:5000')
-                    .send({u: settings.accounts[index].email,p: settings.accounts[index].password})
-    if(result.body.code === 0){
-        _accessToken = result.body.access_token
-    }else{
-        _accessToken = null
-    }
-}
-
-const replyAccessToken = async() => {
-    if(_replyAccessToken <= 3 ){
-        _replyAccessToken++
-        await getAccessToken()
-    }
-}
-
-await getAccessToken()
-setInterval( async()=>{
-    await getAccessToken()
-}, 8 * 60 * 60 * 1000)
-
+const map = new ExpiryMap(3 * 60 * 1000)
+const api = new ChatGPTAPI({
+    apiKey: settings.openai_api_key,
+    messageStore: map
+});
 
 const server = fastify();
 
@@ -70,42 +40,23 @@ server.post('/chatgpt', async (request, reply) => {
     if(!subject){
         return reply.send({ code: 1, msg: 'subject error' })
     }
-    if(_accessToken === null){
-        return res.json({ code: 1, msg: 'accessToken error' })
-    }
-    const conversationId = request.body.conversation_id ? request.body.conversation_id.toString() : undefined;
-
-    const proxyUrl = [
-        'https://chat.duti.tech/api/conversation',
-        // 'https://gpt.pawan.krd/backend-api/conversation'
-    ]
-    const index = Math.floor((Math.random()*proxyUrl.length))
 
     let result;
     let error;
     try{
         const parentMessageId = request.body.parent_message_id ? request.body.parent_message_id.toString() : undefined;
         
-        const api = new ChatGPTUnofficialProxyAPI({
-            accessToken: _accessToken,
-            apiReverseProxyUrl: proxyUrl[index]
-        })
-        
         result = await api.sendMessage(subject, {
-            conversationId,
             parentMessageId,
             timeoutMs: 3 * 60 * 1000, 
         });
-        // console.log(result);
     } catch (e) {
         error = e;
     }
     if (result !== undefined) {
         reply.send({
             content : result.text,
-            conversation_id: result.conversationId,
-            parent_message_id : result.id,
-            server: index
+            parent_message_id : result.id
         });
     } else {
         console.error(error);
--- settings.js
+++ settings.js
@@ -1,11 +1,12 @@
 export default {
     accounts: [
     ],
-    index: '1-10',
+    index: '1-2',
     // The port the server will run on (optional, defaults to 3000)
     port: 8788,
     // Your NopeCHA API key. This will be applied to all accounts but can be overridden on a per-account basis.
     nopechaKey: '',
     // Your 2Captcha API key. This will be applied to all accounts but can be overridden on a per-account basis.
-    twoCaptchaKey: '499ac194a4f199e08e3eee7184bd74d1'
+    twoCaptchaKey: '499ac194a4f199e08e3eee7184bd74d1',
+    openai_api_key: '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'
 }
