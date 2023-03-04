9281194a615c2825f1ead58885de98832fbbbc04
wanglei <317448423@qq.com>
2023-03-04 14:02:16 +0800

    fix bug

--- settings.js
+++ settings.js
@@ -8,5 +8,5 @@ export default {
     nopechaKey: '',
     // Your 2Captcha API key. This will be applied to all accounts but can be overridden on a per-account basis.
     twoCaptchaKey: '499ac194a4f199e08e3eee7184bd74d1',
-    openai_api_key: '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'
+    openai_api_key: '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'
 }
